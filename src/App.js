85fc821bf7b8beb7a18dec6764694e3da9c5517a
ManuelRios94 <manuel.ir2302@gmail.com>
2021-03-13 20:21:16 +0100

    Base Screens

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Dashboard from './components/Dashboard/Dashboard'
+import Pricing from './components/Pricing/Pricing'
+import SingIn from './components/SingIn/SingIn'
+import SingUp from './components/SingUp/SingUp'
+
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/price">Price</Link>
+            </li>
+            <li>
+              <Link to="/sing-up">Sing Up</Link>
+            </li>
+            <li>
+              <Link to="/sing-in">Sing In</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/sing-up">
+            <SingUp />
+          </Route>
+          <Route path="/sing-in">
+            <SingIn />
+          </Route>
+          <Route path="/price">
+            <Pricing />
+          </Route>
+          <Route path="/">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
